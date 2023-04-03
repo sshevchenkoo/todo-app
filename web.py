9a1b3d9a8cbcb7c099d1bf51b2a457be29e4f4ba
shevchenko_yarik <yros22776@gmail.com>
2023-04-03 19:34:18 +0200

    Created web app and gui app

---
+++ web.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import functions
+import os
+
+todos = functions.get_todos()
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", "w") as file:
+        pass
+
+st.title("My todo")
+st.subheader("This is my todo app")
+st.write("This app is to increase your productivity")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
